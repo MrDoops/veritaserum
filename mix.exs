3465bad53604c769b2c587acec28fa8e9f01a7e8
Doops <zkwhited@gmail.com>
2018-07-31 10:00:49 -0600

    replacing Poison with Jason

--- mix.exs
+++ mix.exs
@@ -53,10 +53,10 @@ defmodule Veritaserum.Mixfile do
 
   defp deps do
     [
-      {:poison, "~> 2.0.0", override: true},
       {:credo, "~> 0.3", only: [:dev, :test]},
       {:coverex, "~> 1.4", only: :test},
       {:ex_doc, "~> 0.15.1", only: :dev, runtime: false},
+      {:jason, "~> 1.1.0"},
     ]
   end
 
